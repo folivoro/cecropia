1d8d7ca4424e854162922c8c6f194ed6117d2bc1
Ben Kremer <git@benkremer.de>
2026-04-30 22:09:07 +0200

    feat: Define SLOTH_START for application timing

--- cecropia.php
+++ cecropia.php
@@ -9,6 +9,11 @@
 use Sloth\Core\Application;
 
 /**
+ * Define SLOTH_START for timing
+ */
+defined('SLOTH_START') ? SLOTH_START : define('SLOTH_START', microtime(true));
+
+/**
  * Configure and boot the sloth application container
  */
 Application::configure()->boot();
